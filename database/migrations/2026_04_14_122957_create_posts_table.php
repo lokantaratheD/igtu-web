e7169bbbfda83b43b75f1d7bba830011a8515651
lokantaratheD <lokataradaniel@gmail.com>
2026-04-18 15:53:03 +0700

    Fix: Add missing is_active column to posts table

--- database/migrations/2026_04_14_122957_create_posts_table.php
+++ database/migrations/2026_04_14_122957_create_posts_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->boolean('is_active')->default(true);
+            // memancing git untuk update
         });
     }
 
