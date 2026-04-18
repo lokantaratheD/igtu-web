42d7d36bd82890b6edbaf955f2055dcc49cb901e
lokantaratheD <lokataradaniel@gmail.com>
2026-04-18 15:28:49 +0700

    Fix: Remove missing SetLocale middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,11 +10,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    // ->withMiddleware(function (Middleware $middleware) {
-    //     $middleware->web(append: [
-    //         \App\Http\Middleware\SetLocale::class,
-    //     ]);
-    // })
+    ->withMiddleware(function (Middleware $middleware) {
+
+    })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
