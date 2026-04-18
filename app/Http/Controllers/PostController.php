643eea8884d04073cd49f12dec49699c732c88ab
lokantaratheD <lokataradaniel@gmail.com>
2026-04-18 19:12:24 +0700

    Fix: Update view path show_post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,9 +9,7 @@ class PostController extends Controller
     public function show($slug)
 {
     // Cari postingan berdasarkan slug yang aktif
-    $post = \App\Models\Post::where('slug', $slug)
-                ->where('is_active', true)
-                ->firstOrFail();
+    $post = \App\Models\Post::where('slug', $slug)->firstOrFail();
 
     return view('posts_show', compact('post'));
 }
