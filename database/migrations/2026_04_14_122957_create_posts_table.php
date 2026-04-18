96eda803b4b84ea1cfc990a15c0b1a3b6a70fcee
lokantaratheD <lokataradaniel@gmail.com>
2026-04-18 17:31:27 +0700

    Fix: Add missing title

--- database/migrations/2026_04_14_122957_create_posts_table.php
+++ database/migrations/2026_04_14_122957_create_posts_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('title');
             $table->boolean('is_active')->default(true);
             $table->string('slug')->unique();
             $table->timestamp('published_at')->nullable();
