1a3a482836ffbd24360df8e09974ece0de914584
lokantaratheD <lokataradaniel@gmail.com>
2026-04-18 16:15:49 +0700

    Fix: Add user

--- database/migrations/2026_04_14_122957_create_posts_table.php
+++ database/migrations/2026_04_14_122957_create_posts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->boolean('is_active')->default(true);
-            $table->timestamp('published_at')->nullable();
+           $table->timestamp('published_at')->nullable();
             // memancing git untuk update
         });
     }
--- database/migrations/2026_04_14_123012_create_publications_table.php
+++ database/migrations/2026_04_14_123012_create_publications_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('publications', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->timestamp('publication_year')->nullable();
+            //check
         });
     }
 
