e8e52285b335f074bbb93d5ac9390afd939b21b4
lokantaratheD <lokataradaniel@gmail.com>
2026-04-18 17:20:15 +0700

    Fix: Add missing namespace to SetLocale middleware

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -8,13 +8,18 @@ use Symfony\Component\HttpFoundation\Response;
 
 class SetLocale
 {
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
     public function handle(Request $request, Closure $next): Response
     {
         // Jika ada data 'locale' di session, ubah bahasa aplikasi
         if (session()->has('locale')) {
             app()->setLocale(session('locale'));
         }
-        
+        //check
         return $next($request);
     }
 }
